24dcc072e2f519a9ab8a70d735964bf984170958
Rohit Rehan <rohit@rohitrehan.com>
2022-10-13 22:50:24 +0530

    added ssg

--- src/root.tsx
+++ src/root.tsx
@@ -1,4 +1,4 @@
-import { component$, useStore } from "@builder.io/qwik";
+import { component$ } from "@builder.io/qwik";
 import {
   QwikCity,
   RouterOutlet,
@@ -9,19 +9,10 @@ import { RouterHead } from "./components/router-head/router-head";
 import "./global.css";
 
 export default component$(() => {
-  const { user } = useStore({
-    user: {
-      name: "Rohit",
-      email: "sample",
-      password: "hell",
-      phone: "aaa",
-    },
-  });
   return (
     <QwikCity>
       <head>
         <meta charSet="utf-8" />
-        <meta name="author" content={user.name} />
         <RouterHead />
       </head>
       <body lang="en">
