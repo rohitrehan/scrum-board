2146a0f3da2c6975fe4412dc452f93b44f467e2e
Rohit Rehan <rohit@rohitrehan.com>
2022-10-14 15:01:05 +0530

    netlify

--- src/root.tsx
+++ src/root.tsx
@@ -1,14 +1,16 @@
-import { component$ } from "@builder.io/qwik";
-import {
-  QwikCity,
-  RouterOutlet,
-  ServiceWorkerRegister,
-} from "@builder.io/qwik-city";
-import { RouterHead } from "./components/router-head/router-head";
+import { component$ } from '@builder.io/qwik';
+import { QwikCity, RouterOutlet, ServiceWorkerRegister } from '@builder.io/qwik-city';
+import { RouterHead } from './components/router-head/router-head';
 
-import "./global.css";
+import './global.css';
 
 export default component$(() => {
+  /**
+   * The root of a QwikCity site always start with the <QwikCity> component,
+   * immediately followed by the document's <head> and <body>.
+   *
+   * Dont remove the `<head>` and `<body>` elements.
+   */
   return (
     <QwikCity>
       <head>
